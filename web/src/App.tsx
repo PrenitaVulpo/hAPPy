44bb9760f2a14018a78b4e6fa095ee378143f8e3
PrenitaVulpo <guilhermepdl97@protonmail.com>
2020-10-13 00:21:01 -0300

    added landing page style

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import './styles/global.css';
+import './styles/pages/landing.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" id="page-landing">
       <h1>Hello World</h1>
-      </header>
     </div>
   );
 }
