8804501ac344a690ce9dd24d4054cea495a3f49d
PrenitaVulpo <guilhermepdl97@protonmail.com>
2020-10-13 00:57:45 -0300

    finished the landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import './styles/global.css';
 import './styles/pages/landing.css';
+import logoImg from './img/logo.svg';
+import {FiArrowRight} from 'react-icons/fi';
 
 function App() {
   return (
-    <div className="App" id="page-landing">
-      <h1>Hello World</h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy"/>
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite instituições e mude o dia de muitas crianças</p>
+        </main>
+
+        <div className="location">
+          <strong>Barbalha</strong>
+          <span>Ceará</span>
+        </div>
+
+        <a href="" className="enter-app">
+          <FiArrowRight size={26} color= "rgba(0,0,0,0.6)"/>
+        </a>
+      </div>
     </div>
   );
 }
