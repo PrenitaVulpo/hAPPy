36bb4a6fd344f8acbc443d1f2c55eaf9e242d930
PrenitaVulpo <guilhermepdl97@protonmail.com>
2020-10-14 11:45:39 -0300

    Added backend

--- web/src/pages/map.tsx
+++ web/src/pages/map.tsx
@@ -27,6 +27,8 @@ function MapPage(){
                 style={{
                     width: '100%', height: '100%'}}>
                     <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+                    {/*<TileLayer 
+                    url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/titles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}/>*/}
                 </Map>
             <Link to="" className="create-institution">
                 <FiPlus size={32} color="#fff"/>
