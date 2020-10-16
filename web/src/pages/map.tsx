3332a17373bec307146ba4cda56fade24805f6c4
PrenitaVulpo <guilhermepdl97@protonmail.com>
2020-10-16 12:17:07 -0300

    added icon on the map

--- web/src/pages/map.tsx
+++ web/src/pages/map.tsx
@@ -1,40 +1,49 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import {FiPlus} from 'react-icons/fi'; 
-import {Map, TileLayer} from 'react-leaflet'; 
+import {Map, TileLayer, Marker} from 'react-leaflet';
+import Leaflet from 'leaflet'; 
 import 'leaflet/dist/leaflet.css';
 
 import mapMarkerImg from '../img/Map-marker.svg';
 import '../styles/pages/map-style.css';
 
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMarkerImg
+})
+
 function MapPage(){
-    return(
-        <div id="page-map">
-            <aside>
-                <header>
-                    <img src={mapMarkerImg} alt="happy"/>
-                    <h2>Escolha uma instituição no mapa</h2>
-                    <p>Muitas crianças estão espreando a sua visita :)</p>
-                </header>
-                <footer>
-                    <strong>Barbalha</strong>
-                    <span>Ceará</span>
-                </footer>
-            </aside>
-            <Map
-                center={[-7.3170944,-39.3019392]}
-                zoom={15}
-                style={{
-                    width: '100%', height: '100%'}}>
-                    <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
-                    {/*<TileLayer 
-                    url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/titles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}/>*/}
-                </Map>
-            <Link to="" className="create-institution">
-                <FiPlus size={32} color="#fff"/>
-            </Link>
-        </div>
-    );
+  return(
+      <div id="page-map">
+          <aside>
+              <header>
+                  <img src={mapMarkerImg} alt="happy"/>
+                  <h2>Escolha uma instituição no mapa</h2>
+                  <p>Muitas crianças estão espreando a sua visita :)</p>
+              </header>
+              <footer>
+                  <strong>Barbalha</strong>
+                  <span>Ceará</span>
+              </footer>
+          </aside>
+          <Map
+              center={[-7.3170944,-39.3019392]}
+              zoom={15}
+              style={{
+                  width: '100%', height: '100%'}}>
+                  <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+                  <Marker 
+                  icon={mapIcon}
+                  position={[-7.3170944,-39.3019392]}
+                  />
+                  {/*<TileLayer 
+                  url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/titles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}/>*/}
+              </Map>
+          <Link to="" className="create-institution">
+              <FiPlus size={32} color="#fff"/>
+          </Link>
+      </div>
+  );
 }
 
 export default MapPage;
