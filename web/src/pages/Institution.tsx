49fb12243ed1ba7f29073ad7caeb6a504d4d4275
PrenitaVulpo <guilhermepdl97@protonmail.com>
2020-10-18 01:59:09 -0300

    institution page is halfway through

--- web/src/pages/Institution.tsx
+++ web/src/pages/Institution.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaWhatsapp } from "react-icons/fa";
 import { FiClock, FiInfo } from "react-icons/fi";
 import { Map, Marker, TileLayer } from "react-leaflet";
@@ -6,9 +6,42 @@ import { Map, Marker, TileLayer } from "react-leaflet";
 import '../styles/pages/institution.css';
 import Sidebar from "../components/Sidebar";
 import mapIcon from '../utils/mapIcon';
+import api from "../services/api";
+import { useParams } from "react-router-dom";
+
+interface Institution{
+  id: number;
+  latitude: number;
+  longitude: number;
+  name: string;
+  about: string;
+  instructions: string;
+  opening_hours: string;
+  open_on_weekends: boolean;
+  images: Array<{
+    id: number;
+    url: string;
+  }>
+}
+
+interface routeParams{
+  id: string
+}
 
 
 export default function Orphanage() {
+  const params = useParams<routeParams>()
+  const [institution, setInstitution] = useState<Institution>();
+
+  useEffect(()=>{
+    api.get(`institutions/${params.id}`).then(response => {
+      setInstitution(response.data);
+    })
+  },[params.id]);
+
+  if(!institution){
+    return <p>Carregando...</p>
+  }
 
   return (
     <div id="page-orphanage">
@@ -16,36 +49,23 @@ export default function Orphanage() {
 
       <main>
         <div className="orphanage-details">
-          <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
+          <img src={institution.images[0].url} alt={institution.name} />
 
           <div className="images">
-            <button className="active" type="button">
-              <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
-            </button>
-            <button type="button">
-              <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
-            </button>
-            <button type="button">
-              <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
-            </button>
-            <button type="button">
-              <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
-            </button>
-            <button type="button">
-              <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
-            </button>
-            <button type="button">
-              <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
-            </button>
+            {institution.images.map(image=>{
+              return(<button key={image.id} className="active" type="button">
+                <img src={image.url} alt={institution.name} />
+              </button>)
+            })}
           </div>
           
           <div className="orphanage-details-content">
-            <h1>Lar das meninas</h1>
-            <p>Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.</p>
+            <h1>{institution.name}</h1>
+            <p>{institution.about}</p>
 
             <div className="map-container">
               <Map 
-                center={[-27.2092052,-49.6401092]} 
+                center={[institution.latitude,institution.longitude]} 
                 zoom={16} 
                 style={{ width: '100%', height: 280 }}
                 dragging={false}
@@ -57,30 +77,42 @@ export default function Orphanage() {
                 <TileLayer 
                   url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
                 />
-                <Marker interactive={false} icon={mapIcon} position={[-27.2092052,-49.6401092]} />
+                <Marker interactive={false} icon={mapIcon} position={[institution.latitude,institution.longitude]} />
               </Map>
 
               <footer>
-                <a href="">Ver rotas no Google Maps</a>
+                <a
+                target="_blank"
+                rel="noopener noreferrer" 
+                href={`https://www.google.com/maps/dir/?api=1&destination=${institution.latitude},${institution.longitude}`}>
+                  Ver rotas no Google Maps</a>
               </footer>
             </div>
 
             <hr />
 
             <h2>Instruções para visita</h2>
-            <p>Venha como se sentir mais à vontade e traga muito amor para dar.</p>
+            <p>{institution.instructions}</p>
 
             <div className="open-details">
               <div className="hour">
                 <FiClock size={32} color="#15B6D6" />
-                Segunda à Sexta <br />
-                8h às 18h
+                Segunda a sexta<br/>
+                {institution.opening_hours}
               </div>
-              <div className="open-on-weekends">
+              {institution.open_on_weekends ? (
+                <div className="open-on-weekends">
                 <FiInfo size={32} color="#39CC83" />
                 Atendemos <br />
                 fim de semana
               </div>
+              ) : (
+                <div className="open-on-weekends not-open">
+                <FiInfo size={32} color="#FF669D" />
+                Não atendemos <br />
+                fim de semana
+              </div>
+              )}
             </div>
 
             <button type="button" className="contact-button">
--- web/src/pages/map.tsx
+++ web/src/pages/map.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Link} from 'react-router-dom';
 import {FiPlus, FiArrowRight} from 'react-icons/fi'; 
 import {Map, TileLayer, Marker, Popup} from 'react-leaflet';
@@ -7,10 +7,24 @@ import 'leaflet/dist/leaflet.css';
 import mapMarkerImg from '../img/Map-marker.svg';
 import '../styles/pages/map-style.css';
 import mapIcon from '../utils/mapIcon';
+import api from '../services/api';
 
-
+interface Institution{
+  id: number;
+  latitude: number;
+  longitude: number;
+  name: string;
+}
 
 function MapPage(){
+  const [institutions, setInstitutions] = useState<Institution[]>([]);
+
+  useEffect(()=>{
+    api.get('institutions').then(response => {
+      setInstitutions(response.data);
+    })
+  },[]);
+
   return(
       <div id="page-map">
           <aside>
@@ -28,18 +42,25 @@ function MapPage(){
               center={[-7.3170944,-39.3019392]}
               zoom={15}
               style={{
-                  width: '100%', height: '100%'}}>
-                  <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
-                  <Marker 
-                  icon={mapIcon}
-                  position={[-7.3170944,-39.3019392]}>
-                    <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
-                      Lar das meninas
-                      <Link to="/institutions/1" ><FiArrowRight size={20} color="#FFF"/></Link>
-                    </Popup>  
-                  </Marker>
-                  {/*<TileLayer 
-                  url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/titles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}/>*/}
+            width: '100%', height: '100%'}}>
+                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+                {institutions.map(institution =>{
+                  return(
+                    <Marker 
+                      icon={mapIcon}
+                      position={[institution.latitude,institution.longitude]}
+                      key={institution.id}
+                    >
+                      <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+                        {institution.name}
+                        <Link to={`/institutions/${institution.id}`} ><FiArrowRight size={20} color="#FFF"/></Link>
+                      </Popup>  
+                    </Marker>
+                  )
+                })}
+
+                {/*<TileLayer 
+                url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/titles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}/>*/}
               </Map>
           <Link to="/institutions/create" className="create-institution">
               <FiPlus size={32} color="#fff"/>
