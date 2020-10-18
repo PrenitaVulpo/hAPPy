31b9d489230b3d2b35d0c5aeeb1885731eb42992
PrenitaVulpo <guilhermepdl97@protonmail.com>
2020-10-17 22:27:09 -0300

    added create isntitution page and institution page

--- web/src/pages/map.tsx
+++ web/src/pages/map.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import {FiPlus} from 'react-icons/fi'; 
-import {Map, TileLayer, Marker} from 'react-leaflet';
+import {FiPlus, FiArrowRight} from 'react-icons/fi'; 
+import {Map, TileLayer, Marker, Popup} from 'react-leaflet';
 import Leaflet from 'leaflet'; 
 import 'leaflet/dist/leaflet.css';
 
@@ -9,7 +9,10 @@ import mapMarkerImg from '../img/Map-marker.svg';
 import '../styles/pages/map-style.css';
 
 const mapIcon = Leaflet.icon({
-  iconUrl: mapMarkerImg
+  iconUrl: mapMarkerImg,
+  iconSize: [58,68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170,2],
 })
 
 function MapPage(){
@@ -34,12 +37,16 @@ function MapPage(){
                   <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
                   <Marker 
                   icon={mapIcon}
-                  position={[-7.3170944,-39.3019392]}
-                  />
+                  position={[-7.3170944,-39.3019392]}>
+                    <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+                      Lar das meninas
+                      <Link to="/institutions/7" ><FiArrowRight size={20} color="#FFF"/></Link>
+                    </Popup>  
+                  </Marker>
                   {/*<TileLayer 
                   url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/titles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}/>*/}
               </Map>
-          <Link to="" className="create-institution">
+          <Link to="/institution/create" className="create-institution">
               <FiPlus size={32} color="#fff"/>
           </Link>
       </div>
--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -3,12 +3,16 @@ import {BrowserRouter, Route} from 'react-router-dom';
 //, Switch
 import Landing from './pages/landing';
 import MapPage from "./pages/map";
+import Institution from "./pages/Institution";
+import CreateInstitution from "./pages/CreateInstitution";
 
 function Routes(){
     return(
         <BrowserRouter>
             <Route path="/" exact component={Landing}/>
             <Route path="/app" component={MapPage}/>
+            <Route path="/institution/create" component={Institution}/>
+            <Route path="/institution/:id" component={CreateInstitution}/>
         </BrowserRouter>
 
     )
